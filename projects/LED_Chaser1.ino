3362ab72bc088bc04e7d9586440b400f9f507c90
Arya Bhattacharyya <70765320+xxEasterGrymm@users.noreply.github.com>
2021-08-12 21:52:56 +0530

    Update LED_Chaser1.ino

--- projects/LED_Chaser1.ino
+++ projects/LED_Chaser1.ino
@@ -1,15 +1,11 @@
-void setup()
-{
-  for(int i = 1; i <= 4; i++)
-  {
+void setup() {
+  for(int i = 1; i <= 4; i++) {
     pinMode(i, OUTPUT);
   }
 }
 
-void loop()
-{
-  for(int i = 4; i >= 1; i--)
-  {
+void loop() {
+  for(int i = 4; i >= 1; i--) {
     digitalWrite(i, HIGH);
     delay(1000);
     digitalWrite(i, LOW);
