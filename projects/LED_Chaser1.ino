af99848f0caf234e27c5c715c23658f9d520aa0c
xxEasterGrymm <aryabhattacharyya.study@gmail.com>
2021-08-12 21:48:12 +0530

    initial commit

---
+++ projects/LED_Chaser1.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  for(int i = 1; i <= 4; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop()
+{
+  for(int i = 4; i >= 1; i--)
+  {
+    digitalWrite(i, HIGH);
+    delay(1000);
+    digitalWrite(i, LOW);
+    delay(1000);
+  }
+}
